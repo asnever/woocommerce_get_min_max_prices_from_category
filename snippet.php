d7778eae8f87663f3a9dd0f5b231f9d7fec02421
asnever <44891337+asnever@users.noreply.github.com>
2020-06-11 15:21:07 +0300

    Update snippet.php

--- snippet.php
+++ snippet.php
@@ -7,8 +7,7 @@ $product_cat = $id; // $id - product category id
 $query = "SELECT MIN(meta_value+0) FROM wp_postmeta
             JOIN wp_term_relationships ON wp_term_relationships.object_id = wp_postmeta.post_id
             JOIN wp_term_taxonomy ON wp_term_taxonomy.term_taxonomy_id = wp_term_relationships.term_taxonomy_id
-            JOIN wp_terms ON wp_terms.term_id = wp_term_taxonomy.term_id
-          WHERE wp_postmeta.meta_key = '_price' AND wp_terms.term_id = %d
+          WHERE wp_postmeta.meta_key = '_price' AND wp_term.taxonomy.term_id = %d
           ";
           
 $min_price = $wpdb->get_var( $wpdb->prepare( $query, $id ) );
